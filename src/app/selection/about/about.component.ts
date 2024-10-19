d84a90a46b14f398f5bf708c9fea238bba2fb51c
Luka Jeremic <ljeremic4019rn@raf.rs>
2024-10-19 13:43:44 +0200

    About me page complete

--- src/app/selection/about/about.component.ts
+++ src/app/selection/about/about.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit{
 
+  jobTitle: string = "FullStack"
+
+
+  ngAfterViewInit() {
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      navText: [
+        '<i class="fas fa-chevron-left" style="visibility: hidden;"></i>',
+        '<i class="fas fa-chevron-right" style="visibility: hidden;"></i>'
+      ],
+      responsive: {
+        0: {
+          items: 2
+        },
+        600: {
+          items: 4
+        },
+        1000: {
+          items: 6
+        }
+      }
+    });
+  }
 }
