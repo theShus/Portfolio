f793ced2f25f75590aaa9165795df3f7154cbf25
Luka Jeremic <ljeremic4019rn@raf.rs>
2024-10-27 13:29:40 +0100

    Scroll bug fix

--- src/app/selection/home/home.component.ts
+++ src/app/selection/home/home.component.ts
@@ -18,6 +18,12 @@ export class HomeComponent {
       // Apply the "out" animation based on the chosen type
       this.currentAnimation = this.animationType === 'right' ? 'swipe-right-out' : 'swipe-down-out';
 
+      // Scroll to the top of the right panel when the page changes
+      const rightPanel = document.getElementById('rightPanel');
+      if (rightPanel) {
+        rightPanel.scrollTop = 0;
+      }
+
       setTimeout(() => {
         // Update the selected page and apply the corresponding "in" animation
         this.selectedPage = page;
